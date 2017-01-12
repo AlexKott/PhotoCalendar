06cb156f7fec9943a45e9d9b26335b46b6aa8374
Alex Kott <tetlokax@gmail.com>
2017-01-12 17:13:28 +0100

    chore(webpack): adds LESS support

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,18 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const BUILD_DIR = path.resolve(__dirname, 'dist');
 const APP_DIR = path.resolve(__dirname, 'src');
 
 const JSX_DIR = `${APP_DIR}/jsx`;
+const LESS_DIR = `${APP_DIR}/less`;
 
 module.exports = {
-    entry: `${JSX_DIR}/index.jsx`,
+    entry: [
+        `${JSX_DIR}/index.jsx`,
+        `${LESS_DIR}/main.less`
+    ],
     output: {
         path: `${BUILD_DIR}/js`,
         filename: 'bundle.js'
@@ -17,8 +22,16 @@ module.exports = {
             {
                 test: /\.jsx?/,
                 include: APP_DIR,
+                exclude: /less/,
                 loader: 'babel'
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
             }
         ]
-    }
+    },
+    plugins: [
+        new ExtractTextPlugin('../css/style.css')
+    ]
 };
