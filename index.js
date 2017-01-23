50c5525cc46d8d5b7dd2543dcdb196439f9b2e7a
Alex Kott <tetlokax@gmail.com>
2017-01-23 22:36:23 +0100

    refactor(api): extract router to own file

--- index.js
+++ index.js
@@ -1,43 +1,13 @@
 const express = require('express');
 const app = express();
 
-const calendarService = require('./services/calendarService');
-const photoService = require('./services/photoService');
+const travelRouter = require('./router.js');
 
 const port = 4040;
 
 app.use(express.static('dist'));
 
-app.get('/events', (req, res) => {
-    if (Object.keys(req.query).length === 0) {
-        return res.status(400).send('Missing query parameters');
-    }
-    calendarService.getEventsByMonth(req.query.month)
-        .then(events => res.send(events))
-        .catch(error => res.send(error));
-});
-
-app.get('/entries', (req, res) => {
-    if (Object.keys(req.query).length === 0) {
-        return res.status(400).send('Missing query parameters');
-    }
-
-    let getPhotos;
-    if ({}.hasOwnProperty.call(req.query, 'month')) {
-        getPhotos = () => photoService.getPhotosByMonth(req.query.month);
-    } else if ({}.hasOwnProperty.call(req.query, 'selectedDate')) {
-        getPhotos = () => photoService.getPhotosByDay(req.query.selectedDate);
-    } else {
-        getPhotos = () => photoService.getPhotosByPeriod(req.query.startDate, req.query.endDate);
-    }
-
-    getPhotos()
-        .then((photos) => res.send(photos))
-        .catch((error) => {
-            console.log(error);
-            res.send(error)
-        });
-});
+app.use('/', travelRouter);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
---
+++ router.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const calendarService = require('./services/calendarService');
+const photoService = require('./services/photoService');
+
+router.get('/events', (req, res) => {
+    if (Object.keys(req.query).length === 0) {
+        return res.status(400).send('Missing query parameters');
+    }
+    calendarService.getEventsByMonth(req.query.month)
+        .then(events => res.send(events))
+        .catch(error => res.send(error));
+});
+
+router.get('/entries', (req, res) => {
+    if (Object.keys(req.query).length === 0) {
+        return res.status(400).send('Missing query parameters');
+    }
+
+    let getPhotos;
+    if ({}.hasOwnProperty.call(req.query, 'month')) {
+        getPhotos = () => photoService.getPhotosByMonth(req.query.month);
+    } else if ({}.hasOwnProperty.call(req.query, 'selectedDate')) {
+        getPhotos = () => photoService.getPhotosByDay(req.query.selectedDate);
+    } else {
+        getPhotos = () => photoService.getPhotosByPeriod(req.query.startDate, req.query.endDate);
+    }
+
+    getPhotos()
+        .then((photos) => res.send(photos))
+        .catch((error) => {
+            console.log(error);
+            res.send(error)
+        });
+});
+
+module.exports = router;
