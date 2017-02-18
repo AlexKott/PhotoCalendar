8f5ef8b0688a42a26d370546b39f91be119fb4f7
Alex Kott <tetlokax@gmail.com>
2017-02-18 18:07:00 +0100

    feat(newsletter): newsletter sent to email list in mongodb

---
+++ newsletter/emailService.js
@@ -0,0 +1,40 @@
+module.exports = {
+    sendEmail(recipients, calendarUpdates, photoUpdates) {
+        sendmail({
+            from: 'Alina and Alex <no-reply@alexkott.com>',
+            to: recipients.map(r => r.email).join(', '),
+            subject: 'New updates from Alina and Alex',
+            html: formatEmail(calendarUpdates, photoUpdates)
+        }, (err, reply) => {
+            if (err) {
+                console.log(err && err.stack);
+            } else {
+                console.log(`Newsletter sent at ${endDate}.`);
+            }
+        });
+    }
+};
+
+function formatEmail(calendarUpdates, photoUpdates) {
+    const head = fs.readFileSync(path.join(__dirname, 'head.html'));
+    const foot = fs.readFileSync(path.join(__dirname, 'foot.html'))
+    let body = '';
+
+    if (calendarUpdates.length === 1) {
+        body += '<h2>A new destination has been chosen!</h2>';
+        body += `<p>We added <strong>${calendarUpdates[0].summary}</strong> and will probably be there from ${calendarUpdates[0].startDate} until ${calendarUpdates[0].endDate}.</p>`;
+    } else if (calendarUpdates > 1) {
+        const destinations = calendarUpdates.map(a => a.summary).join(' and ');
+        body += '<h2>Some new destination have been chosen!</h2>';
+        body += `<p>We added <strong>${destinations}</strong>. How exciting!</p>`;
+    }
+    if (photoUpdates) {
+        const photoDates = Object.keys(photoUpdates);
+        const examplePhoto = photoUpdates[photoDates[0]].media[0];
+        body += '<h2>We added some new photos</h2>';
+        body += `<p>In the last week we added ${photoDates.length} new photos. Here's a teaser:</p>`;
+        body += `<img src="${examplePhoto.path}/${examplePhoto.fileName}" width="${examplePhoto.width}" height="${examplePhoto.height}" />`;
+    }
+    body += '<p>Go check it out at <a href="https://travel.alexkott.com">the website</a></p>';
+    return head + body + foot;
+}
--- newsletter/index.js
+++ newsletter/index.js
@@ -2,7 +2,31 @@ const fs = require('fs');
 const path = require('path');
 const photoService = require('../services/photoService');
 const calendarService = require('../services/calendarService');
+const emailService = require('./emailService');
 const sendmail = require('sendmail')();
+const MongoClient = require('mongodb').MongoClient;
+const { dbUser, dbPassword, dbDomain } = require('../mongoConfig.json');
+
+const dbUrl = `mongodb://${dbUser}:${dbPassword}@${dbDomain}`;
+
+const fetchMailAddresses = new Promise((resolve, reject) => {
+    MongoClient.connect(dbUrl, (connectionError, db) => {
+        if (!connectionError) {
+            db
+                .collection('newsletterEmails')
+                .find({}, { _id: 0 }, (dbError, cursor) => {
+                    if (!dbError) {
+                        resolve(cursor.toArray());
+                        db.close();
+                    } else {
+                        reject(dbError);
+                    }
+                });
+        } else {
+            reject(connectionError);
+        }
+    });
+});
 
 const updatePromises = [];
 
@@ -12,57 +36,23 @@ startDate.setDate(endDate.getDate() - 7);
 
 updatePromises.push(calendarService.getAllEvents({ updatedMin: startDate }));
 updatePromises.push(photoService.getPhotosByPeriod(startDate, endDate));
+updatePromises.push(fetchMailAddresses);
 
 Promise.all(updatePromises)
     .then((updates) => {
-        const photoUpdates = updates[1];
-
         const calendarUpdates = updates[0].filter((update) => {
             if (update.created < startDate.toISOString()) {
                 return false;
             }
             return true;
         });
+        const photoUpdates = updates[1];
+        const recipients = updates[2];
 
         if (calendarUpdates.length || photoUpdates) {
-            const html = formatEmail(calendarUpdates, photoUpdates);
-            sendEmail(html);
+            emailService.sendEmail(recipients, calendarUpdates, photoUpdates);
+        } else {
+            console.log(`No news, no newsletter at ${endDate}.`);
         }
     })
     .catch(error => console.log(error));
-
-function formatEmail(calendarUpdates, photoUpdates) {
-    const head = fs.readFileSync(path.join(__dirname, 'head.html'));
-    const foot = fs.readFileSync(path.join(__dirname, 'foot.html'))
-    let body = '';
-
-    if (calendarUpdates.length === 1) {
-        body += '<h2>A new destination has been chosen!</h2>';
-        body += `<p>We added <strong>${calendarUpdates[0].summary}</strong> and will probably be there from ${calendarUpdates[0].startDate} until ${calendarUpdates[0].endDate}.</p>`;
-    } else if (calendarUpdates > 1) {
-        const destinations = calendarUpdates.map(a => a.summary).join(' and ');
-        body += '<h2>Some new destination have been chosen!</h2>';
-        body += `<p>We added <strong>${destinations}</strong>. How exciting!</p>`;
-    }
-    if (photoUpdates) {
-        const photoDates = Object.keys(photoUpdates);
-        const examplePhoto = photoUpdates[photoDates[0]].media[0];
-        body += '<h2>We added some new photos</h2>';
-        body += `<p>In the last week we added ${photoDates.length} new photos. Here's a teaser:</p>`;
-        body += `<img src="${examplePhoto.path}/${examplePhoto.fileName}" width="${examplePhoto.width}" height="${examplePhoto.height}" />`;
-    }
-    body += '<p>Go check it out at <a href="https://travel.alexkott.com">the website</a></p>';
-    return head + body + foot;
-}
-
-function sendEmail(html) {
-    sendmail({
-        from: 'no-reply@alexkott.com',
-        to: 'alex.kott@mail.com',
-        subject: 'New updates from Alina and Alex',
-        html
-    }, (err, reply) => {
-        console.log(err && err.stack);
-        console.dir(reply);
-    });
-}
