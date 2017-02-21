3fc297ac57037716c10e465fcf925d50bb1430d2
Alex Kott <tetlokax@gmail.com>
2017-02-21 19:36:48 +0100

    fix(newsletter): remove breaking encoding parameter

--- newsletter/emailService.js
+++ newsletter/emailService.js
@@ -3,17 +3,15 @@ const path = require('path');
 const sendmail = require('sendmail')({ silent: process.env.NODE_ENV === 'production' });
 const doT = require('dot');
 
-doT.templateSettings = { strip: true };
-
 module.exports = {
     sendEmail(recipients, calendarUpdates, photoUpdates) {
+        const from = 'Alina and Alex <no-reply@alexkott.com>';
+        const to = recipients.map(r => r.email).join(', ');
+        const subject = 'New updates from Alina and Alex';
+        const html = buildEmail(calendarUpdates, photoUpdates);
         const now = new Date();
-        sendmail({
-            from: 'Alina and Alex <no-reply@alexkott.com>',
-            to: recipients.map(r => r.email).join(', '),
-            subject: 'New updates from Alina and Alex',
-            html: buildEmail(calendarUpdates, photoUpdates)
-        }, (err, reply) => {
+
+        sendmail({ from, to, subject, html }, (err, reply) => {
             if (err) {
                 console.log(err && err.stack);
             } else {
@@ -24,7 +22,7 @@ module.exports = {
 };
 
 function buildEmail(calendarUpdates, photoUpdates) {
-    const template = fs.readFileSync(path.join(__dirname, 'template.jst'), 'utf-8');
+    const template = fs.readFileSync(path.join(__dirname, 'template.jst'));
     const templateFunction = doT.template(template);
 
     const photoDates = Object.keys(photoUpdates);
