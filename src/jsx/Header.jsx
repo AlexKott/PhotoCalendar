efa5f53b3ebc3489d87d59d2548052a24cf28955
AndroidAva <4ndroid4va@gmail.com>
2017-02-24 21:56:30 +0100

    feat(Header): adds calendar icon svg and basic style

--- src/jsx/Header.jsx
+++ src/jsx/Header.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import IconCalendar from './svg/IconCalendar.jsx';
 
 class Header extends React.Component {
     render() {
         return(
             <div className="header">
-                {!this.props.isCalendarActive && <button className="button" onClick={() => this.props.showCalendar()}>Show Calendar</button>}
+                {!this.props.isCalendarActive &&
+                    <button
+                        className="button button--header"
+                        onClick={() => this.props.showCalendar()}
+                    ><IconCalendar /></button>}
                 <h1 className="header__title">{this.props.title}</h1>
+                <button className="button button--header button--header-info">i</button>
             </div>
         )
     }
