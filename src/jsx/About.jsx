4cddbae66df33573d101035383205ef0a4aa80b4
AndroidAva <4ndroid4va@gmail.com>
2017-03-22 12:39:46 +0400

    refactor(About): cleans up component code

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 
-class About extends React.Component {
-    render() {
-        return (
-            <div className="textbox">
-                <h1>Hello there!</h1>
-                <p>We're Alex and Alina. If you didn't know that already you probably went wrong somewhere in the world wide web and there's a nice little button up left in your browser that will help you find your way back.</p>
-                <p>Otherwise, you're very welcome!</p>
-                <p>On this page you will find all there is to know about our travel-the-world-remote-working-adventure: pictures, plans (as far as we have any) and stories. If you have travel plans yourself and would like to meet us somewhere, please let us know!</p>
-                <p>Also, if you find pictures of yourself that you would like us to remove you may hand in a formal complaint in quadruplicate that we will gladly review as fast as we can. You know, within the following six months or so. And know this: We love you, no matter how stupid you look in a picture. That's why you're on here.</p>
-            </div>
-        );
-    }
+export default function About() {
+    return (
+        <div className="textbox">
+            <h1>Hello there!</h1>
+            <p>We're Alex and Alina. If you didn't know that already you probably went wrong somewhere in the world wide web and there's a nice little button up left in your browser that will help you find your way back.</p>
+            <p>Otherwise, you're very welcome!</p>
+            <p>On this page you will find all there is to know about our travel-the-world-remote-working-adventure: pictures, plans (as far as we have any) and stories. If you have travel plans yourself and would like to meet us somewhere, please let us know!</p>
+            <p>Also, if you find pictures of yourself that you would like us to remove you may hand in a formal complaint in quadruplicate that we will gladly review as fast as we can. You know, within the following six months or so. And know this: We love you, no matter how stupid you look in a picture. That's why you're on here.</p>
+        </div>
+    );
 }
-
-export default About;
