af81b4e84ddd648595a6953347d376e2a6a5dfad
AndroidAva <4ndroid4va@gmail.com>
2017-03-22 16:00:26 +0400

    refactor(NavButton): cleans up component code

--- src/jsx/NavButton.jsx
+++ src/jsx/NavButton.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 
-class NavButton extends React.Component {
-    render() {
-        const classNames = `button button--nav button--nav-${this.props.direction}`;
-        return (
-            <button onClick={this.props.onClick} className={classNames}>&#9654;</button>
-        )
-    }
+export default function NavButton({ direction, onClick }) {
+    const classNames = `button button--nav button--nav-${direction}`;
+    return (
+        <button onClick={onClick} className={classNames}>&#9654;</button>
+    );
 }
-
-export default NavButton;
