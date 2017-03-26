edd46dd325781e97ad3c9a9f2cd17ecccfa7c60c
AndroidAva <4ndroid4va@gmail.com>
2017-03-26 19:34:59 +0400

    feat(design): using same transition for all clickable images

--- src/jsx/calendar/CalendarWeek.jsx
+++ src/jsx/calendar/CalendarWeek.jsx
@@ -4,22 +4,19 @@ export default function CalendarWeek({ week, thumbnails, onSelectDay }) {
     return (
         <div className="week__days">
             {week.map((day, index) => {
-                if (day.date) {
-                    return (
-                        <div className="week__day" key={index}>
-                            {thumbnails[day.date] &&
-                                <div
-                                    className="week__day-thumbnail"
-                                    style={{ backgroundImage: `url(${thumbnails[day.date][0].thumbnailSrc})`}}
-                                    onClick={() => onSelectDay(day.date)}
-                                />
-                            }
-                            <span className="week__day-number">{day.displayNumber}</span>
-                        </div>
-                    );
-                } else {
-                    return (<div className="week__day week__day--pseudo" key={index}></div>);
-                }
+                const classNames = `week__day ${day.date ? '' : 'week__day--pseudo'} ${thumbnails[day.date] ? 'week__day--hasContent' : ''}`;
+                return (
+                    <div className={classNames}>
+                        {thumbnails[day.date] &&
+                            <div
+                                className="week__day-thumbnail"
+                                style={{ backgroundImage: `url(${thumbnails[day.date][0].thumbnailSrc})`}}
+                                onClick={() => onSelectDay(day.date)}
+                            />
+                        }
+                        <span className="week__day-number">{day.displayNumber}</span>
+                    </div>
+                );
             })}
         </div>
     );
