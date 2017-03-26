6ee0a42af10a8327b45b187cef021a792fe76b3f
AndroidAva <4ndroid4va@gmail.com>
2017-03-26 19:34:36 +0400

    feat(design): small design improvements in header

--- src/jsx/app/Header.jsx
+++ src/jsx/app/Header.jsx
@@ -17,20 +17,20 @@ export default function Header({
         <div className="header">
             {!isCalendarActive &&
                 <button
-                    className="button header__button header__button--calendar"
+                    className="button button--header button--header--left"
                     onClick={() => onShowCalendar()}
                 ><IconCalendar size={iconSize} /></button>
             }
             <h1 className="header__title">{title}</h1>
             {!isAboutActive &&
                 <button
-                    className="button header__button header__button--info"
+                    className="button button--header"
                     onClick={() => onShowAbout()}
                 ><IconQuestion size={iconSize} /></button>
             }
             {isAboutActive &&
                 <button
-                    className="button header__button header__button--info"
+                    className="button button--header"
                     onClick={() => onHideAbout()}
                 ><IconClose size={iconSize} /></button>
             }
