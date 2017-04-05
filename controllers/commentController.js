d4ad681b6c48e5a0855b187bd3133b752d7f3cd7
Alex Kott <tetlokax@gmail.com>
2017-04-05 17:53:15 +0400

    feat(comments): adds more design to the notification

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -46,11 +46,12 @@ module.exports = {
                                     .map(c => `${c.authorName} <${c.authorEmail}>`);
                                 recipients.push('Alex Kott <alex.kott@mail.com>');
 
-                                let content = '<html><head><title>New Comment</title></head><body>';
-                                content += `<p><strong>${authorName}</strong> replied to your comment:</p>`;
+                                let content = mailHead;
+                                content += '<h1>New comments</h1>';
+                                content += `<h2><strong>${authorName}</strong> replied to your comment:</h2>`;
                                 content += html;
-                                content += `<p><a href="https://travel.alexkott.com${slug}">Click here</a>`;
-                                content += ` or enter <pre style="display:inline">travel.alexkott.com${slug}</pre> in your browser.</p>`;
+                                content += `<p><a href="https://travel.alexkott.com${slug}">Click here</a> to see it on the website.</p>`;
+                                content += mailFoot;
                                 recipients.forEach(to => newsletterService.sendNotification(to, subject, content));
                             });
                         })
@@ -67,3 +68,18 @@ module.exports = {
             });
     }
 }
+
+// Temporary solution, use jst here.
+const mailHead = `<html><head><meta charset="utf-8"><title>New Comment</title><style>
+    body { width: 100%; color: #222222; font-family: Helvetica, Arial, sans-serif; margin: 0; padding 0; }
+    .wrapper { width: 100%; background-color: #777777; padding: 20px 0; }
+    .content { width: 100%; max-width: 600px; margin: 0 auto; padding: 20px; background-color: #FBFBFB; }
+    h1 { font-size: 24px; margin: 0 0 16px; padding: 4px 8px; background-color: #880011; color: #FBFBFB; }
+    h2 { font-size: 18px; margin: 20px 0 4px 0; border-left: 4px solid #880011; padding-left: 4px; }
+    p, li { font-size: 14px; line-height: 1.46; margin: 8px 0 8px; padding: 0 8px }
+    a { color: #880011; text-decoration: none; border-bottom: 1px solid currentColor; }
+    a:hover { border: none; }
+    .small { font-size: 10px; padding-left: 8px; }
+    hr { width: 40%; margin: 20px auto; border: 1px solid #880011; border-top: none; }
+</style></head><body><div class="wrapper"><div class="content">`;
+const mailFoot = `</div></div></body></html>`;
